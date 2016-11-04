710844d38759db11e02dafcded04494b4545343e
levihernan <levi.hernan@gmail.com>
2016-11-04 18:41:41 -0300

    Orden en .js

--- js/buildSeries.js
+++ js/buildSeries.js
@@ -7,5 +7,11 @@ var datasets = [{
     "type": "area",
     "valueDecimals": 2
 }];
-
+serie1 = [{
+    "name": "SERIE",
+    "data": combinedArray,
+    "unit": "$$$",
+    "type": "column",
+    "valueDecimals": 2
+}];
 }
