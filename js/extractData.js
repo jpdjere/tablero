a9c976dcc3b80bd8683da03847475967e0a09cd1
Juan Pablo Djeredjian <jpdjeredjian@gmail.com>
2016-11-07 15:23:41 -0300

    Completed extractData.js

--- js/extractData.js
+++ js/extractData.js
@@ -1,9 +1,8 @@
 var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/11P1uupLHALlVtTXUAOm_MJclPvYGx5PX_AgjSsJiPKg/pubhtml'; //Funciona
 
-var date = [];
-var exp = [];
-var imp = [];
-var combinedArray = [];
+
+var series = {date:[], exportaciones:[], exportaciones_var:[], importaciones:[],  reservas:[], precio_soja:[], indice_construya:[], actividad:[], actividad_desest:[]};
+
 
 function renderSpreadsheetData() {
   Tabletop.init( { key: public_spreadsheet_url,
@@ -17,22 +16,26 @@ function draw(data, tabletop) {
   console.log("Data:");
   console.log(data);
   // render the table(s)
-  //tabulate(data, ['date', 'exportaciones','importaciones']); // 2 column table
-
 
   for(var i = 0;i<data.length;i++){
-    date.push(data[i].date);
-    tempDate = data[i].date.split("/");
-    combinedArray.push([Date.UTC(tempDate[2], tempDate[1], tempDate[0]), parseFloat(data[i]['Precio de la soja'])]);
-    exp.push(parseFloat(data[i].exportaciones));
-    imp.push(parseFloat(data[i].importaciones));
+    Object.keys(data[i]).forEach(function(key,index) {
+      
+      date.push(data[i].date);
+      tempDate = data[i].date.split("/");
 
+      if(data[0][key] !== "0"){
+        series[key].push([Date.UTC(tempDate[2], tempDate[1], tempDate[0]), parseFloat(data[i][key])]);
+      };
+
+    });
   }
-  console.log("combinedArray:");
-  console.log(combinedArray);
+
   buildSeries();
   drawCharts();
 
 }
 
 renderSpreadsheetData();
+
+
+
