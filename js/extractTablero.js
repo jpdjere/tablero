ea474c9f94ff8d8b3c36b071d35799172179a4de
Juan Pablo Djeredjian <jpdjeredjian@gmail.com>
2016-11-09 16:50:07 -0300

    Added Tablero prototype

--- js/extractTablero.js
+++ js/extractTablero.js
@@ -1,17 +1,6 @@
 var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1VSGksDN0dnCGHCVdozWm4JwCR8YbI0jYmHmqEynmLhM/pubhtml'; //Funciona
 
-var columns = [
-    { head: 'Indicador', cl: 'title',
-      html: function(row) { return r.title; } },
-    { head: 'Year', cl: 'center',
-      html: function(row) { return r.year; } },
-    { head: 'Length', cl: 'center',
-      html: function(row) { return r.length; } },
-    { head: 'Budget', cl: 'num',
-      html: function(row) { return r.budget; } },
-    { head: 'Rating', cl: 'num',
-      html: function(row) { return r.rating; } }
-];
+var filledColumns = ['indicador', 'periodo', 'valor','variacion_ia', 'val_2016','var_16_15']
 
 function renderSpreadsheetData() {
   Tabletop.init( { key: public_spreadsheet_url,
@@ -24,17 +13,81 @@ function draw(dataTablero, tabletop) {
 	// draw chart
 	console.log("Data Tablero:");
 	console.log(dataTablero);
-	// render the table(s)
 
-	
-	var tr = d3.select('#sectorExterno')
-	  .data(dataTablero).enter();
+  function tabulate(data, selection, columns) {
+    //var table = d3.select('#sectorExterno').append('table').attr("style","width:100%")
+    var table = selection.append('table').attr("style","width:100%")
+    var thead = table.append('thead')
+    var tbody = table.append('tbody');
 
-	tr.append('td').html(function(d) { return d.indicador; });
-	tr.append('td').html(function(d) { return d.valor; });
-	tr.append('td').html(function(d) { return d.variacion_ia; });
-	tr.append('td').html(function(d) { return d.val_2016; });
-	tr.append('td').html(function(d) { return d.var_16_15; });
+    // // append the header row
+    // thead.append('tr')
+    //   .selectAll('th')
+    //   .data(columns).enter()
+    //   .append('th')
+    //     .text(function (column) { return column; });
+
+    // create a row for each object in the data
+    var rows = tbody.selectAll('tr')
+      .data(data)
+      .enter()
+      .append('tr');
+
+    // create a cell in each row for each column
+    var cells = rows.selectAll('td')
+      .data(function (row) {
+
+        return columns.map(function (column) {
+          console.log("-----------------");
+          console.log("row");
+          console.log(row);
+          console.log("column");
+          console.log(column);
+          console.log("row[column]");
+          console.log(row[column]);
+          if(jQuery.inArray(column,filledColumns) !== -1){
+            return {column: column, value: row[column]};
+          }else{
+            return {column: column, value: "free space"};
+          }
+        });
+      })
+      .enter()
+      .append('td')
+        .text(function (d) { return d.value; });
+
+    return table;
+  }	
+
+  // render the table(s)
+  //las columnas las tengo que incluir para guardar el espacio para ellas, incluso a las que no les paso data
+  dataSectorExt = dataTablero.slice(0,6);
+  selection = d3.select('#sectorExterno');
+  tabulate(dataSectorExt, selection, ['indicador', 'unidad','periodo', 'valor','variacion_ia', 'periodo2','val_2016','var_16_15']); // 2 column table
+
+  dataInversion = dataTablero.slice(6,12);
+  selection = d3.select('#inversion');
+  tabulate(dataInversion, selection, ['indicador', 'unidad','periodo', 'valor','variacion_ia', 'periodo2','val_2016','var_16_15']); // 2 column table
+
+  dataConsumo = dataTablero.slice(12,17);
+  selection = d3.select('#consumo');
+  tabulate(dataConsumo, selection,  ['indicador', 'unidad','periodo', 'valor','variacion_ia', 'periodo2','val_2016','var_16_15']); // 2 column table
+
+  dataPrecios = dataTablero.slice(17,23);
+  selection = d3.select('#precios');
+  tabulate(dataPrecios, selection,  ['indicador', 'unidad','periodo', 'valor','variacion_ia', 'periodo2','val_2016','var_16_15']); // 2 column table
+
+  dataSectorReal = dataTablero.slice(23,33);
+  selection = d3.select('#sectorReal');
+  tabulate(dataSectorReal,  selection, ['indicador', 'unidad','periodo', 'valor','variacion_ia', 'periodo2','val_2016','var_16_15']); // 2 column table
+
+  dataMonetario = dataTablero.slice(33,36);
+  selection = d3.select('#monetario');
+  tabulate(dataMonetario,  selection, ['indicador', 'unidad','periodo', 'valor','variacion_ia', 'periodo2','val_2016','var_16_15']); // 2 column table
+
+  dataFiscal = dataTablero.slice(36,40);
+  selection = d3.select('#fiscal');
+  tabulate(dataFiscal, selection,  ['indicador', 'unidad','periodo', 'valor','variacion_ia', 'periodo2','val_2016','var_16_15']); // 2 column table
 
 }
 
